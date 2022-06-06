2691b3c1b9882965280f46d3dfe9d4985e2493fe
ChucEnvy <dumydumpdump@gmail.com>
2022-06-06 23:14:11 +0700

    add action bar

--- app/src/main/java/com/example/sololingo/MainActivity.java
+++ app/src/main/java/com/example/sololingo/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.sololingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void bindingView() {
+
+    }
+
+    private void bindingAction() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setMainBar();
+    }
+
+
+    private void setMainBar() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_menudark);
+            getSupportActionBar().setTitle(R.string.app_name);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
