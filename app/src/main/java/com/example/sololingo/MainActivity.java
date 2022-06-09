1cdffc520cea85bccfa5b8f63e27fe25d99275de
ChucEnvy <dumydumpdump@gmail.com>
2022-06-09 21:23:27 +0700

    test part 1

--- app/src/main/java/com/example/sololingo/MainActivity.java
+++ app/src/main/java/com/example/sololingo/MainActivity.java
@@ -7,33 +7,14 @@ import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void bindingView() {
 
-    }
-
-    private void bindingAction() {
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setMainBar();
     }
 
 
-    private void setMainBar() {
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_menudark);
-            getSupportActionBar().setTitle(R.string.app_name);
-        }
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mainmenu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 }
